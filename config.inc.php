7e8527284123b18cccde9d6511da8f29e72fa126
NlightN22 <oncharterliz@gmail.com>
2024-01-14 00:37:54 +0700

    add config

---
+++ config.inc.php
@@ -0,0 +1,12 @@
+<?php
+##
+## custom Button configuration file
+##
+
+// URL
+$config['custom_button_url'] = 'https://your_domain.com/user';
+// _blank - for new page or empty for current page
+$config['custom_button_target'] = '';
+$config['custom_button_label'] = 'User';
+
+?>
