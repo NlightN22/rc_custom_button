85d1c3dc40deed70983d478c3d5a4fea5978d421
NlightN22 <oncharterliz@gmail.com>
2024-01-14 00:35:46 +0700

    change all

---
+++ custom_button.php
@@ -0,0 +1,32 @@
+<?php
+
+class custom_button extends rcube_plugin
+{
+	private $url;
+
+	function init()
+	{
+		$this->load_config();
+		$this->url = rcube::get_instance()->config->get('custom_button_url', '');
+		$this->target = rcube::get_instance()->config->get('custom_button_target', '');
+		$this->label = rcube::get_instance()->config->get('custom_button_label', '');
+
+		if (!$this->url) {
+			return;
+		}
+
+		$this->include_stylesheet($this->local_skin_path() . '/custom_button.css');
+
+		$this->add_button(array(
+			'type'       => 'link',
+			'content'    => $this->label,
+			'href'       => $this->url,
+			'target'     => $this->target,
+			'class'      => 'button-custom',
+			'classsel'   => 'button-custom button-selected',
+			'innerclass' => 'button-inner'
+		), 'taskbar');
+	}
+}
+
+?>
